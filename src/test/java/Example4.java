82d08106988bf0695de5fb6cbb6aa08cd7df469c
nagamanirr <rajprabha74@gmail.com>
2019-05-10 00:53:48 +0530

    Example4 Phone Number and Type print

--- src/test/java/Example4.java
+++ src/test/java/Example4.java
@@ -1,8 +1,10 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
 
 public class Example4 {
     public static void main(String[] args) {
@@ -15,6 +17,16 @@ public class Example4 {
 
           //write code here to print all the type of phone no and the corresponding no
 
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String,?> person = (Map<String, ?>) obj;
+            Person person1 = objectMapper.readValue(person.toString(),Person.class);
+            System.out.println(person1.phoneNumber);
+
+            System.out.println("Done");
+
+
+         //   System.out.println("printing Entire Person Details as well");
+         //   System.out.println(person1);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
